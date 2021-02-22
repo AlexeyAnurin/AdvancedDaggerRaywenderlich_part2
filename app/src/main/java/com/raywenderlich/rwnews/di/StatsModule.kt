8fa37a6024b6fd3ed0919e3ccb15645841c793d1
AlexeyAnurin <Anurin.Alexey@gmail.com>
2021-02-22 16:33:55 +0200

    fun provideNewsStats() @IntoSet instead of @ElementsIntoSet

--- app/src/main/java/com/raywenderlich/rwnews/di/StatsModule.kt
+++ app/src/main/java/com/raywenderlich/rwnews/di/StatsModule.kt
@@ -1,14 +1,23 @@
 package com.raywenderlich.rwnews.di
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.ElementsIntoSet
+import dagger.multibindings.IntoSet
 
 @Module
 class StatsModule {
 
-    @Provides
+   /* @Provides
     @ElementsIntoSet
     fun provideNewsStats(): Set<NewsStats> {
-        return setOf(LengthNewsStats()) }
+        return setOf(LengthNewsStats()) }*/
+
+    @Provides
+    @IntoSet
+    fun provideNewsStats(): NewsStats {
+        return LengthNewsStats() }
+
 }
+
